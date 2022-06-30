7e70a4d55f8d73069ada45fb5def0e1ee9d7071c
arikawaaaaaaaaa <taisei-arikawa-k21h@stu.kbc.ac.jp>
2022-06-30 12:37:40 +0900

    applegameのプロパティ
    applegameのプロパティを編集しました
    これでも動かなかったら行ってね

--- applegame/applegame/Player.h
+++ applegame/applegame/Player.h
@@ -6,11 +6,6 @@ private:
 	double angle;	//機体の向き
 	int count;		//タイミング用
 	int speed;		//移動速度
-	int hp;			//体力
-	int fuel;		//燃料
-	int bari;		//バリア回数
-	int baricnt;	//バリア継続時間
-	int bariup;		//バリア回数(走行距離アップ)
 
 public:
 	int flg;		//使用フラグ
